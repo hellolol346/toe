056119c26f51b623ef2b9dc52b2fccc6d52a7708
meows <meow@iceland>
2020-10-11 12:12:37 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    [null, null, ""],
+    [null, null, null]
+  ])
+  const [current, setCurrent] = useState("X")
+  const [condition, setConditin] = useState("ongoing") 
+
+  const sample = [["X", "O", "X"], [null, "X", "O"], ["O", null, null]]
+
   return (
     <div className="App">
       <main>
         <section>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+          <div className="cell" onClick={() => setBoard(sample)}>{board[0][0]}</div>
+          <div className="cell" onClick={() => 3}>{board[0][1]}</div>
+          <div className="cell" onClick={() => 3}>{board[0][2]}</div>
         </section>
         <section>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
+          <div className="cell" onClick={() => 3}>{board[1][0]}</div>
+          <div className="cell" onClick={() => 3}>{board[1][1]}</div>
+          <div className="cell" onClick={() => 3}>{board[1][2]}</div>
         </section>
         <section>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
+          <div className="cell" onClick={() => 3}>{board[2][0]}</div>
+          <div className="cell" onClick={() => 3}>{board[2][1]}</div>
+          <div className="cell" onClick={() => 3}>{board[2][2]}</div>
         </section>
       </main>
     </div>
